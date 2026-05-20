417effce9ff23d1b9b97a04427e2bc970954af41
Roman-orn <0671635566@ukr.net>
2026-05-20 21:51:55 +0300

    Added functionality to the Main class

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,4 +1,20 @@
 package app;
 
+import java.util.List;
+
 public class Main {
+
+    static void main(String[] arg){
+
+        DataRepository repository = new DataRepository();
+        DataHandler handler = new DataHandler();
+        UIOperator operator = new UIOperator();
+
+        List<String> data = repository.getData();
+        String listOutput = handler.listOutput(data);
+        String elementAt = handler.elementOutputAt(data, 2);
+
+        operator.getOutput(listOutput);
+        operator.getOutput(elementAt);
+    }
 }
