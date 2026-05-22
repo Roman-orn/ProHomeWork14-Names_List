2cf1df1a42ee950ed1817df2824f33a5d05f66e9
Roman-orn <0671635566@ukr.net>
2026-05-22 21:45:24 +0300

    Fix Main class

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,17 +4,26 @@ import java.util.List;
 
 public class Main {
 
-    static void main(String[] arg){
+    static void main(String[] arg) {
 
         DataRepository repository = new DataRepository();
         DataHandler handler = new DataHandler();
         UIOperator operator = new UIOperator();
 
         List<String> data = repository.getData();
-        String listOutput = handler.listOutput(data);
-        String elementAt = handler.elementOutputAt(data, 2);
 
-        operator.getOutput(listOutput);
-        operator.getOutput(elementAt);
+        try {
+            String listOutput = handler.listOutput(data);
+            operator.getOutput(listOutput);
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            String elementAt = handler.elementOutputAt(data, 2);
+            operator.getOutput(elementAt);
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
